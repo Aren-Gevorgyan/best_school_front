a550f1dfe5b36ae7d6aa528ac9892514b8a4d9e2
Aren-Gevorgyan <areng4518@gmail.com>
2022-02-19 19:56:28 +0400

    create modal

--- common/createOption/index.js
+++ common/createOption/index.js
@@ -2,8 +2,10 @@ import styles from "./styles.module.scss";
 import PropTypes from "prop-types";
 import CreateItem from "../createItem";
 import { useState } from "react";
-import { Modal } from "antd";
+import { Form, Modal } from "antd";
+import Upload from "../upload";
 import SvgClose from "../svgIcons/Close";
+import TextArea from "antd/lib/input/TextArea";
 
 const CreateOption = ({ options }) => {
   const [isModalVisible, setIsModalVisible] = useState(false);
@@ -30,7 +32,42 @@ const CreateOption = ({ options }) => {
           setIsModalVisible(false);
         }}
       >
-        <div className={styles.contentModal}></div>
+        <div className={styles.contentModal}>
+          <Form
+            onFinish={(e) => {
+              console.log(e);
+            }}
+          >
+            <h3>Option title</h3>
+            <Form.Item name="optionTitle">
+              <TextArea
+                className={styles.textArea}
+                rows={4}
+                placeholder="option title"
+                maxLength={200}
+              />
+            </Form.Item>
+
+            <h3>Item title</h3>
+            <Form.Item name="optionTitle">
+              <TextArea
+                className={styles.textArea}
+                rows={4}
+                placeholder="item title"
+                maxLength={200}
+              />
+            </Form.Item>
+
+            <h3>Item image</h3>
+            <Form.Item name="optionTitle">
+              <Upload
+                onLoad={(e) => {
+                  console.log(e, "eeeee");
+                }}
+              />
+            </Form.Item>
+          </Form>
+        </div>
       </Modal>
     </div>
   );
